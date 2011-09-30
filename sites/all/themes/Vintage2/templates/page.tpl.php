9bf5074369d776c086e65618e7d3388e6bfab4d1
David Moore <davidmoore@david-moores-macbook-pro.local>
2011-09-30 17:11:50 -0400

    more bug tracking and wine page byob comment

--- sites/all/themes/Vintage2/templates/page.tpl.php
+++ sites/all/themes/Vintage2/templates/page.tpl.php
@@ -34,6 +34,9 @@
     <?php endif; ?>
 </div>
 
+<div class="reservations_widget_box">
+<script type="text/javascript" src="http://www.opentable.com/frontdoor/default.aspx?rid=51451&restref=51451&bgcolor=F6F6F3&titlecolor=0F0F0F&subtitlecolor=0F0F0F&btnbgimage=http://www.opentable.com/frontdoor/img/ot_btn_red.png&otlink=FFFFFF&icon=dark&mode=tall&hover=1"></script><a href="http://www.opentable.com/vintage-enoteca-reservations-los-angeles?rtype=ism&restref=51451" class="OT_ExtLink">Vintage Enoteca Reservations</a>
+</div>
 </div>
 
 <?php if ($messages): ?>
@@ -93,6 +96,13 @@
   
 
 </div>
+
+    <?php if ($page['pagecomment']): ?>
+   <div class="grid-12 alpha omega pagecomment">
+      <?php print render($page['pagecomment']); ?>
+      </div>
+  <?php endif; ?>
+  
 </div>
 
 <div id="sub_node_box">
